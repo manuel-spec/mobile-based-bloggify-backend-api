993e8db246e5e4e96b0c0f76baf1c8deeea6ab1c
manuel-spec <amaniase084@gmail.com>
2024-06-03 22:11:54 -0700

    comment controller done

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,10 +1,53 @@
-<?php
-
-namespace App\Http\Controllers;
+<?php 
 
 use Illuminate\Http\Request;
+use App\Models\Comment; // Assuming you have a Comment model
 
 class CommentController extends Controller
 {
-    //
+  
+    public function index()
+    {
+        $comments = Comment::all();
+        return response()->json($comments);
+    }
+
+    public function store(Request $request)
+    {
+        $comment = new Comment;
+        $comment->content = $request->input('content');
+        $comment->save();
+        return response()->json($comment);
+    }
+
+    public function show($id)
+    {
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        return response()->json($comment);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        $comment->content = $request->input('content');
+        $comment->save();
+        return response()->json($comment);
+    }
+
+
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        $comment->delete();
+        return response()->json(['message' => 'Comment deleted']);
+    }
 }
