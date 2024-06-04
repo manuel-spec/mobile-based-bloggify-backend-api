f7fc33a739b6fb33f2d9d84841bbf5cde31f64f1
manuel-spec <amaniase084@gmail.com>
2024-06-03 22:15:15 -0700

    contorller updated

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,53 +1,80 @@
-<?php 
+<?php
+
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Comment; // Assuming you have a Comment model
+use App\Models\Comment;
+use App\Models\BlogModel;
 
 class CommentController extends Controller
 {
-  
-    public function index()
+    public function __construct()
     {
-        $comments = Comment::all();
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
+    public function index($blogId)
+    {
+        $comments = Comment::where('blog_id', $blogId)->get();
         return response()->json($comments);
     }
 
-    public function store(Request $request)
+    public function store(Request $request, $blogId)
     {
+        $request->validate([
+            'content' => 'required|string|max:255',
+        ]);
+
+        $blog = BlogModel::find($blogId);
+        if (!$blog) {
+            return response()->json(['message' => 'Blog not found'], 404);
+        }
+
         $comment = new Comment;
         $comment->content = $request->input('content');
+        $comment->blog_id = $blogId;
+        $comment->user_id = auth()->id(); // Assuming comments are related to users
         $comment->save();
-        return response()->json($comment);
+
+        return response()->json($comment, 201);
     }
 
-    public function show($id)
+    public function show($blogId, $id)
     {
-        $comment = Comment::find($id);
+        $comment = Comment::where('blog_id', $blogId)->find($id);
         if (!$comment) {
             return response()->json(['message' => 'Comment not found'], 404);
         }
+
         return response()->json($comment);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $blogId, $id)
     {
-        $comment = Comment::find($id);
+        $request->validate([
+            'content' => 'required|string|max:255',
+        ]);
+
+        $comment = Comment::where('blog_id', $blogId)->find($id);
         if (!$comment) {
             return response()->json(['message' => 'Comment not found'], 404);
         }
+
         $comment->content = $request->input('content');
         $comment->save();
+
         return response()->json($comment);
     }
 
-
-    public function destroy($id)
+    public function destroy($blogId, $id)
     {
-        $comment = Comment::find($id);
+        $comment = Comment::where('blog_id', $blogId)->find($id);
         if (!$comment) {
             return response()->json(['message' => 'Comment not found'], 404);
         }
+
         $comment->delete();
+
         return response()->json(['message' => 'Comment deleted']);
     }
 }
